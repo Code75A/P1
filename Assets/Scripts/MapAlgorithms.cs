60272c0a69694a285c5626e6d2ea928da0d1d318
dream-tentacle <127286614+dream-tentacle@users.noreply.github.com>
2023-05-10 11:55:31 +0800

    优化随机: 可自定义种子

--- Assets/Scripts/MapAlgorithms.cs
+++ Assets/Scripts/MapAlgorithms.cs
@@ -2,37 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class MapAlgorithms
+public class MapAlgorithms
 {
-    public static HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int WalkLength)
+    public HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int WalkLength)
     {
         HashSet<Vector2Int> path = new HashSet<Vector2Int>();
         Vector2Int currentPosition = startPosition;
         path.Add(currentPosition);
         for (int i = 0; i < WalkLength; i++)
         {
-            Vector2Int direction = Direction2D.RandomDirection();
+            Vector2Int direction = RandomDirection();
             currentPosition += direction;
             path.Add(currentPosition);
         }
         return path;
     }
 
-}
-
-public static class Direction2D
-{
-    public static List<Vector2Int> directions = new List<Vector2Int>()
+    public List<Vector2Int> directions = new List<Vector2Int>()
     {
         Vector2Int.up,
         Vector2Int.down,
         Vector2Int.left,
         Vector2Int.right
     };
-    public static Vector2Int RandomDirection()
+    public Vector2Int RandomDirection()
     {
-        return directions[Random.Range(0, directions.Count)];
+        return directions[MyRandom.random(0, directions.Count)];
+    }
+    public void setSeed(int seed)
+    {
+        MyRandom.seed(seed);
     }
 }
+
 
 
